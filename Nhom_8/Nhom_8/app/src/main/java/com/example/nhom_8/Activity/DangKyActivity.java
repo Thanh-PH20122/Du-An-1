107789f7c7f2014640ba497bc837ee39b68ed24a
Lam Duc Phuong <phuongldph19688@fpt.edu.vn>
2022-11-16 14:41:40 +0700

    abc

--- Nhom_8/Nhom_8/app/src/main/java/com/example/nhom_8/Activity/DangKyActivity.java
+++ Nhom_8/Nhom_8/app/src/main/java/com/example/nhom_8/Activity/DangKyActivity.java
@@ -6,11 +6,11 @@ import android.os.Bundle;
 
 import com.example.nhom_8.R;
 
-public class MainActivity extends AppCompatActivity {
+public class DangKyActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_dang_ky);
     }
 }
---
+++ Nhom_8/Nhom_8/app/src/main/java/com/example/nhom_8/Activity/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.nhom_8.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.nhom_8.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button = (Button) findViewById(R.id.btnDangKy);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, DangKyActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
