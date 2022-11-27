cb2957615e6722e2a45ed568cbd6d91d2e0f1ec3
LamDucPhuong <phuongldph19688@fpt.edu.vn>
2022-11-27 20:16:12 +0700

    Giao diện quản trị viên

---
+++ QuanTriVien/app/src/main/java/com/example/nhom_8/fragmentQuanLy/QuanLyTK_Fragment.java
@@ -0,0 +1,22 @@
+package com.example.nhom_8.fragmentQuanLy;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.nhom_8.R;
+
+public class QuanLyTK_Fragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_quan_ly_tk, container, false);
+        return view;
+
+    }
+}
